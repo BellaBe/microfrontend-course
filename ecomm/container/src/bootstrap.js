5ef874129df7e4f4e56695bde1a725e587d982ce
bellabe <bella.belgarokova@hotmail.com>
2024-06-02 10:27:48 +0400

    initial commit

---
+++ ecomm/container/src/bootstrap.js
@@ -0,0 +1,10 @@
+import { mount as mountProduct } from 'products/ProductsIndex';
+
+import { mount as mountCart } from 'cart/CartShow';
+
+
+
+console.log('Container!');
+
+mountProduct(document.querySelector('#my-products'));
+mountCart(document.querySelector('#my-cart'));
